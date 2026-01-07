51aa37847c126fbea8a14d47182f46235dca8a48
Ayushi <51916580+IshuDeve@users.noreply.github.com>
2026-01-07 23:15:08 +0530

    Checked that the calculator handles empty input .

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -1,1 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ayushitigihrdemo/string_calculator.dart';
+
+void main() {
+  final cal = StringCalculator();
+
+  test('returns 0 for empty string', () {
+    expect(cal.add(''), equals(0));
+  });
+}
 
